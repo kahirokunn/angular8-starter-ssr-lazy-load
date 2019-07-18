9d490d1710f2da52a202aac8331daed6f8067dfc
su1fur <ihukcl.9748@gmail.com>
2019-07-18 22:32:14 +0900

    Add test component and component-module

--- src/app/page/index/index.module.ts
+++ src/app/page/index/index.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { IndexComponent } from './component/index.component';
 import { FormsModule } from '@angular/forms';
 import { SomeComponent } from '@/app/component/some-component/some.component';
+import { TestComponent } from "@/app/component/test/test.component";
 
 @NgModule({
   declarations: [
     IndexComponent,
     SomeComponent,
+    TestComponent,
   ],
   imports: [
     CommonModule,
